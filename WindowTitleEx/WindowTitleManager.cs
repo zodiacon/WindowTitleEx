ed81191203ef7f44c8a9ef53f7a058cb229bab6c
Pavel Yosifovich <zodiacon@live.com>
2017-09-04 14:50:38 +0300

    added simple exclusion dialog

--- WindowTitleEx/WindowTitleManager.cs
+++ WindowTitleEx/WindowTitleManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,25 +10,42 @@ using WindowTitleEx.Properties;
 namespace WindowTitleEx {
     class WindowTitleManager {
         NotifyIcon _tray;
+        List<string> _excludeProcesses = new List<string>(4);
+        HashSet<int> _excludePids = new HashSet<int>();
+        string _exlcudeProcessesText;
 
         public void Run() {
-            _tray = new NotifyIcon();
+            _excludePids.Add(Process.GetCurrentProcess().Id);
+            var revertTitlesAndCloseMenuItem = new MenuItem {
+                Text = "Revert Titles and Close",
+            };
+            var closeMenuItem = new MenuItem {
+                Text = "Close"
+            };
+            var settingsMenu = new MenuItem {
+                Text = "Settings...",
+                DefaultItem = true
+            };
             var menu = new ContextMenu {
                 MenuItems = {
-                    new MenuItem {
-                        Text = "Revert Titles and Close",
-                    },
-                    new MenuItem {
-                        Text = "Close"
-                    }
+                    revertTitlesAndCloseMenuItem, closeMenuItem,
+                    new MenuItem { Text = "-" },
+                    settingsMenu
                 }
             };
-            menu.MenuItems[0].Click += OnRevertAndClose;
-            menu.MenuItems[1].Click += OnClose;
-            _tray.ContextMenu = menu;
-            _tray.Text = "Window Title Ex";
-            _tray.Icon = Resources.app;
-            _tray.Visible = true;
+
+            revertTitlesAndCloseMenuItem.Click += OnRevertAndClose;
+            closeMenuItem.Click += OnClose;
+            settingsMenu.Click += OnSettings;
+
+            _tray = new NotifyIcon {
+                ContextMenu = menu,
+                Text = "Window Title Ex",
+                Icon = Resources.app,
+                Visible = true,
+            };
+
+            _tray.MouseDoubleClick += (s, e) => OnSettings(s, e);
 
             _timer = new System.Windows.Forms.Timer {
                 Interval = 3000,
@@ -37,9 +55,21 @@ namespace WindowTitleEx {
             Application.Run();
         }
 
-        private void OnRevertAndClose(object sender, EventArgs e) {
-            _timer.Enabled = false;
-            _timer.Dispose();
+        private void OnSettings(object sender, EventArgs e) {
+            using (var form = new SettingsForm(_exlcudeProcessesText)) {
+                if (form.ShowDialog() == DialogResult.OK) {
+                    _exlcudeProcessesText = form.ExcludedProcessesText;
+
+                    _excludeProcesses = _exlcudeProcessesText.Split(new[] { '\n', '\r' }, 
+                        StringSplitOptions.RemoveEmptyEntries).ToList();
+                    _excludePids.Clear();
+                    _excludePids.Add(Process.GetCurrentProcess().Id);
+                    RevertWindows();
+                }
+            }
+        }
+
+        void RevertWindows() {
             var text = new StringBuilder(512);
             NativeMethods.EnumWindows((hWnd, p) => {
                 if (NativeMethods.IsWindowVisible(hWnd)) {
@@ -47,7 +77,7 @@ namespace WindowTitleEx {
                     if (text.Length > 0) {
                         var stext = text.ToString();
                         int index = stext.IndexOf("(HWND: ");
-                        if(index >= 0) {
+                        if (index >= 0) {
                             stext = stext.Substring(0, index - 1);
                             NativeMethods.SetWindowText(hWnd, stext);
                         }
@@ -55,6 +85,12 @@ namespace WindowTitleEx {
                 }
                 return true;
             }, IntPtr.Zero);
+        }
+
+        private void OnRevertAndClose(object sender, EventArgs e) {
+            _timer.Enabled = false;
+            RevertWindows();
+            _timer.Dispose();
             _tray.Dispose();
             Application.Exit();
         }
@@ -67,16 +103,26 @@ namespace WindowTitleEx {
             Application.Exit();
         }
 
-        private static void OnTick(object sender, EventArgs e) {
+        private void OnTick(object sender, EventArgs e) {
             var text = new StringBuilder(512);
             NativeMethods.EnumWindows((hWnd, p) => {
                 if (NativeMethods.IsWindowVisible(hWnd)) {
+                    int pid;
+                    int tid = NativeMethods.GetWindowThreadProcessId(hWnd, out pid);
+                    if (_excludePids.Contains(pid))
+                        return true;
+                    if (_excludeProcesses.Count > 0) {
+                        var process = Process.GetProcessById(pid);
+                        if (_excludeProcesses.Contains(process.ProcessName.ToLower())) {
+                            _excludePids.Add(pid);
+                            return true;
+                        }
+                    }
+
                     NativeMethods.GetWindowText(hWnd, text, text.Capacity);
                     if (text.Length > 0) {
                         var stext = text.ToString();
                         if (stext.LastIndexOf("PID: ") < 0) {
-                            int pid;
-                            int tid = NativeMethods.GetWindowThreadProcessId(hWnd, out pid);
                             text.Append($" (HWND: 0x{hWnd:X}, TID: {tid}, PID: {pid})");
                             NativeMethods.SetWindowText(hWnd, text.ToString());
                         }
