07ec29aeba7cb988f9de71979fed7d954ae2a945
Pavel Yosifovich <zodiacon@live.com>
2018-12-13 17:55:11 +0200

    bug fox where some windows cause hang in SetWindowText

--- WindowTitleEx/NativeMethods.cs
+++ WindowTitleEx/NativeMethods.cs
@@ -9,21 +9,36 @@ using System.Threading.Tasks;
 namespace WindowTitleEx {
     delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr param);
 
+	struct Rect {
+		public int Left, Top, Right, Bottom;
+
+		public int Width => Right - Left;
+		public int Height => Bottom - Top;
+	}
+
     [SuppressUnmanagedCodeSecurity]
     static class NativeMethods {
+		const int WM_SETTEXT = 0xc;
+
         [DllImport("user32")]
         public static extern bool EnumWindows(EnumWindowsProc proc, IntPtr param);
 
         [DllImport("user32")]
         public static extern bool IsWindowVisible(IntPtr hWnd);
 
-        [DllImport("user32")]
+        [DllImport("user32", CharSet= CharSet.Unicode)]
         public static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int maxSize);
 
         [DllImport("user32")]
         public static extern int GetWindowThreadProcessId(IntPtr hWnd, out int pid);
 
-        [DllImport("user32")]
+		[DllImport("user32")]
+		public static extern bool GetWindowRect(IntPtr hWnd, out Rect rc);
+
+		[DllImport("user32", CharSet = CharSet.Unicode)]
         public static extern bool SetWindowText(IntPtr hWnd, string text);
-    }
+
+		[DllImport("user32", CharSet = CharSet.Unicode)]
+		public static extern int SendMessageTimeout(IntPtr hWnd, int message, IntPtr wParam, string lparam, int timeout, out IntPtr result);
+	}
 }
--- WindowTitleEx/WindowTitleManager.cs
+++ WindowTitleEx/WindowTitleManager.cs
@@ -8,129 +8,134 @@ using System.Windows.Forms;
 using WindowTitleEx.Properties;
 
 namespace WindowTitleEx {
-    class WindowTitleManager {
-        NotifyIcon _tray;
-        List<string> _excludeProcesses = new List<string>(4);
-        HashSet<int> _excludePids = new HashSet<int>();
-        string _exlcudeProcessesText;
+	class WindowTitleManager {
+		NotifyIcon _tray;
+		List<string> _excludeProcesses = new List<string>(4);
+		HashSet<int> _excludePids = new HashSet<int>();
+		string _exlcudeProcessesText;
 
-        public void Run() {
-            _excludePids.Add(Process.GetCurrentProcess().Id);
-            var revertTitlesAndCloseMenuItem = new MenuItem {
-                Text = "Revert Titles and Close",
-            };
-            var closeMenuItem = new MenuItem {
-                Text = "Close"
-            };
-            var settingsMenu = new MenuItem {
-                Text = "Settings...",
-                DefaultItem = true
-            };
-            var menu = new ContextMenu {
-                MenuItems = {
-                    revertTitlesAndCloseMenuItem, closeMenuItem,
-                    new MenuItem { Text = "-" },
-                    settingsMenu
-                }
-            };
+		public void Run() {
+			_excludePids.Add(Process.GetCurrentProcess().Id);
+			var revertTitlesAndCloseMenuItem = new MenuItem {
+				Text = "Revert Titles and Close",
+			};
+			var closeMenuItem = new MenuItem {
+				Text = "Close"
+			};
+			var settingsMenu = new MenuItem {
+				Text = "Settings...",
+				DefaultItem = true
+			};
+			var menu = new ContextMenu {
+				MenuItems = {
+					revertTitlesAndCloseMenuItem, closeMenuItem,
+					new MenuItem { Text = "-" },
+					settingsMenu
+				}
+			};
 
-            revertTitlesAndCloseMenuItem.Click += OnRevertAndClose;
-            closeMenuItem.Click += OnClose;
-            settingsMenu.Click += OnSettings;
+			revertTitlesAndCloseMenuItem.Click += OnRevertAndClose;
+			closeMenuItem.Click += OnClose;
+			settingsMenu.Click += OnSettings;
 
-            _tray = new NotifyIcon {
-                ContextMenu = menu,
-                Text = "Window Title Ex",
-                Icon = Resources.app,
-                Visible = true,
-            };
+			_tray = new NotifyIcon {
+				ContextMenu = menu,
+				Text = "Window Title Ex",
+				Icon = Resources.app,
+				Visible = true,
+			};
 
-            _tray.MouseDoubleClick += (s, e) => OnSettings(s, e);
+			_tray.MouseDoubleClick += (s, e) => OnSettings(s, e);
 
-            _timer = new System.Windows.Forms.Timer {
-                Interval = 3000,
-                Enabled = true
-            };
-            _timer.Tick += OnTick;
-            Application.Run();
-        }
+			_timer = new System.Windows.Forms.Timer {
+				Interval = 3000,
+				Enabled = true
+			};
+			_timer.Tick += OnTick;
+			Application.Run();
+		}
 
-        private void OnSettings(object sender, EventArgs e) {
-            using (var form = new SettingsForm(_exlcudeProcessesText)) {
-                if (form.ShowDialog() == DialogResult.OK) {
-                    _exlcudeProcessesText = form.ExcludedProcessesText;
+		private void OnSettings(object sender, EventArgs e) {
+			using (var form = new SettingsForm(_exlcudeProcessesText)) {
+				if (form.ShowDialog() == DialogResult.OK) {
+					_exlcudeProcessesText = form.ExcludedProcessesText;
 
-                    _excludeProcesses = _exlcudeProcessesText.Split(new[] { '\n', '\r' }, 
-                        StringSplitOptions.RemoveEmptyEntries).ToList();
-                    _excludePids.Clear();
-                    _excludePids.Add(Process.GetCurrentProcess().Id);
-                    RevertWindows();
-                }
-            }
-        }
+					_excludeProcesses = _exlcudeProcessesText.Split(new[] { '\n', '\r' }, 
+						StringSplitOptions.RemoveEmptyEntries).ToList();
+					_excludePids.Clear();
+					_excludePids.Add(Process.GetCurrentProcess().Id);
+					RevertWindows();
+				}
+			}
+		}
 
-        void RevertWindows() {
-            var text = new StringBuilder(512);
-            NativeMethods.EnumWindows((hWnd, p) => {
-                if (NativeMethods.IsWindowVisible(hWnd)) {
-                    NativeMethods.GetWindowText(hWnd, text, text.Capacity);
-                    if (text.Length > 0) {
-                        var stext = text.ToString();
-                        int index = stext.IndexOf("(HWND: ");
-                        if (index >= 0) {
-                            stext = stext.Substring(0, index - 1);
-                            NativeMethods.SetWindowText(hWnd, stext);
-                        }
-                    }
-                }
-                return true;
-            }, IntPtr.Zero);
-        }
+		void RevertWindows() {
+			var text = new StringBuilder(512);
+			NativeMethods.EnumWindows((hWnd, p) => {
+				if (NativeMethods.IsWindowVisible(hWnd)) {
+					NativeMethods.GetWindowText(hWnd, text, text.Capacity);
+					if (text.Length > 0) {
+						var stext = text.ToString();
+						int index = stext.IndexOf("(HWND: ");
+						if (index >= 0) {
+							stext = stext.Substring(0, index - 1);
+							NativeMethods.SetWindowText(hWnd, stext);
+						}
+					}
+				}
+				return true;
+			}, IntPtr.Zero);
+		}
 
-        private void OnRevertAndClose(object sender, EventArgs e) {
-            _timer.Enabled = false;
-            RevertWindows();
-            _timer.Dispose();
-            _tray.Dispose();
-            Application.Exit();
-        }
+		private void OnRevertAndClose(object sender, EventArgs e) {
+			try {
+				_timer.Enabled = false;
+				RevertWindows();
+				_timer.Dispose();
+				_tray.Dispose();
+			}
+			finally {
+				Application.Exit();
+			}
+		}
 
-        System.Windows.Forms.Timer _timer;
+		System.Windows.Forms.Timer _timer;
 
-        private void OnClose(object sender, EventArgs e) {
-            _timer.Dispose();
-            _tray.Dispose();
-            Application.Exit();
-        }
+		private void OnClose(object sender, EventArgs e) {
+			_timer.Dispose();
+			_tray.Dispose();
+			Application.Exit();
+		}
 
-        private void OnTick(object sender, EventArgs e) {
-            var text = new StringBuilder(512);
-            NativeMethods.EnumWindows((hWnd, p) => {
-                if (NativeMethods.IsWindowVisible(hWnd)) {
-                    int pid;
-                    int tid = NativeMethods.GetWindowThreadProcessId(hWnd, out pid);
-                    if (_excludePids.Contains(pid))
-                        return true;
-                    if (_excludeProcesses.Count > 0) {
-                        var process = Process.GetProcessById(pid);
-                        if (_excludeProcesses.Contains(process.ProcessName.ToLower())) {
-                            _excludePids.Add(pid);
-                            return true;
-                        }
-                    }
+		private void OnTick(object sender, EventArgs e) {
+			var text = new StringBuilder(512);
+			NativeMethods.EnumWindows((hWnd, p) => {
+				if (NativeMethods.IsWindowVisible(hWnd)) {
+					int pid;
+					int tid = NativeMethods.GetWindowThreadProcessId(hWnd, out pid);
+					if (_excludePids.Contains(pid))
+						return true;
+					if (_excludeProcesses.Count > 0) {
+						var process = Process.GetProcessById(pid);
+						if (_excludeProcesses.Contains(process.ProcessName.ToLower())) {
+							_excludePids.Add(pid);
+							return true;
+						}
+					}
 
-                    NativeMethods.GetWindowText(hWnd, text, text.Capacity);
-                    if (text.Length > 0) {
-                        var stext = text.ToString();
-                        if (stext.LastIndexOf("PID: ") < 0) {
-                            text.Append($" (HWND: 0x{hWnd:X}, TID: {tid}, PID: {pid})");
-                            NativeMethods.SetWindowText(hWnd, text.ToString());
-                        }
-                    }
-                }
-                return true;
-            }, IntPtr.Zero);
-        }
+					NativeMethods.GetWindowText(hWnd, text, text.Capacity);
+					NativeMethods.GetWindowRect(hWnd, out var rc);
+					if (rc.Width > 50 && rc.Height > 1 && text.Length > 0) {
+						var stext = text.ToString();
+						if (stext.LastIndexOf("PID: ") < 0) {
+							text.Append($" (HWND: 0x{hWnd:X}, TID: {tid}, PID: {pid})");
+							NativeMethods.SetWindowText(hWnd, text.ToString());
+						}
+					}
+				}
+				return true;
+			}, IntPtr.Zero);
+		}
 
-    }
+	}
 }
